314555479683d341eaa37e8422a87e2638211333
Leeming <874187203@qq.com>
2017-06-23 16:59:00 +0800

    Create NormalThread2.java
    运行结果
    a:5
    b:4
    b:3
    a:2
    a:1

---
+++ src/main/java/thread/NormalThread2.java
@@ -0,0 +1,30 @@
+package default;
+/**
+ * Created by lixiaoming on 2017/6/23.
+ */
+public class NormalThread2 implements Runnable {
+
+    private volatile int count = 5;
+
+    @Override
+    public void run() {
+        while (count > 0) {
+            System.out.println(Thread.currentThread().getName() + ":" + count--);
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Runnable runnable = new NormalThread2();
+
+        Thread t1 = new Thread(runnable, "a");
+        Thread t2 = new Thread(runnable, "b");
+
+        t1.start();
+        t2.start();
+    }
+}
